6e393713bbaaf3f80ea19889b297f7f8e2443c59
Bolton <lbolton@sun.ac.za>
2026-03-09 09:07:29 +0200

    add licensing

--- scripts/wisca_demo.R
+++ scripts/wisca_demo.R
@@ -1,3 +1,19 @@
+# Copyright (c) 2026 Larisse Bolton
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 library(tidyverse)
 library(AMR)
 library(lubridate)
@@ -17,7 +33,7 @@ data_in <- data_raw %>%
          age_at_onset = as.numeric(age_at_onset),
          sex = as.factor(sex),
          birthweight_g = as.numeric(birthweight_g),
-         infection_type = as.factor(infection_type)) 
+         infection_type = as.factor(infection_type))
 
 
 antibiotic_in_demo <- c("amp+gent","meropenem","piptaz+amikacin","meropenem+vancomycin") #what antibiotics are you wanting to include in the wisca
@@ -29,14 +45,14 @@ include_pathogens <- 10
 source("./Code/wisca_preprocess_amr.R")  #script to run preprocessing
 
 data_wisca_demo <- wisca_preprocess(x = data_in,
-                                   antibiotic_in = antibiotic_in_demo, 
+                                   antibiotic_in = antibiotic_in_demo,
                                    pathogen_in = include_pathogens,
                                    exclude = exclude_path_demo,
                                    susceptible_I = susceptible_I_in,
                                    isolate_first = deduped)
 
 antibiotic_wisca_demo <- data_wisca_demo[[1]]
-data_wisca_in_demo <- data_wisca_demo[[2]] 
+data_wisca_in_demo <- data_wisca_demo[[2]]
 
 
 
@@ -44,7 +60,7 @@ data_wisca_in_demo <- data_wisca_demo[[2]]
 ##RUNNING WISCA
 wisca_cover_demo <- data_wisca_in_demo %>%
   group_by(INFECTION_TYPE) %>%
-  wisca(#x = data_wisca_in, 
+  wisca(#x = data_wisca_in,
     antimicrobials = antibiotic_wisca_demo,
     col_mo = "mo",
     ab_transform = NULL)
@@ -62,13 +78,13 @@ wisca_cover_demo_plot <- wisca_cover_demo %>%
          hospital = as.factor(hospital)) %>%
   mutate(across(.cols = "estimate":"u_ci", .fns = as.numeric))
 
-ggplot(wisca_cover_demo_plot, aes(x = keyantimicrobials)) + 
+ggplot(wisca_cover_demo_plot, aes(x = keyantimicrobials)) +
   geom_col(aes(y = estimate, fill = INFECTION_TYPE), position = "dodge") +
-  geom_errorbar(aes(ymin = l_ci, ymax = u_ci, group = hospital), color = "black",position = position_dodge(width = 0.9), 
+  geom_errorbar(aes(ymin = l_ci, ymax = u_ci, group = hospital), color = "black",position = position_dodge(width = 0.9),
                 width = 0.1, linewidth = 1) +
   theme_bw() +
   theme(text = element_text(family = "sans", size = 14),
-        axis.text = element_text(size = 14)) + 
+        axis.text = element_text(size = 14)) +
   labs(title = paste0("Average coverage for top ",include_pathogens," of circulating pathogens"),
        x = "Antibiotic regimens",
        y = "Coverage (95% credible intervals) (%)",
--- scripts/wisca_preprocess_amr.R
+++ scripts/wisca_preprocess_amr.R
@@ -1,9 +1,25 @@
+# Copyright (c) 2026 Larisse Bolton
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_I, isolate_first){
-  
+
   # Define antibiotic regimens under investigation from "antibiotics" dataset
   antibiotic_df <- function(abo_in){
     mono_ab <- str_to_title(abo_in[!str_detect(abo_in,fixed("+"))]) #identify single antibiotics
-    
+
     mono_antibiotic_x_set <- data.frame()
     for (j in 1:length(mono_ab)){ #generate dataframe for single antibiotics with abbreviations and fullnames
       mono_x_name <- ab_name(mono_ab[j], only_first = TRUE)
@@ -13,12 +29,12 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
     }
     names(mono_antibiotic_x_set) <- c("ab1","namerx")
     mono_antibiotic_x_set$ab1 <- as.character(mono_antibiotic_x_set$ab1)
-    
-    
+
+
     comb_ab <- str_to_title(abo_in[str_detect(abo_in,fixed("+"))]) #identify combination regimens
     if(length(comb_ab) > 0){
-      
-      comb_antibiotic_x_total <- data.frame()    
+
+      comb_antibiotic_x_total <- data.frame()
       for (jj in 1:length(comb_ab)){ #generate dataframe for combination antibiotics with abbreviations and fullnames
         comb_ab_sep <- unlist(str_split(comb_ab[jj], fixed("+")))
         comb_antibiotic_x_pre <- data.frame(x = rep(0, times = 2), y = rep(0, times = 2))
@@ -37,21 +53,21 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
         names(comb_antibiotic_x_set)[ncol(comb_antibiotic_x_set)]<- "namerx"
         comb_antibiotic_x_total <-rbind.data.frame(comb_antibiotic_x_total, comb_antibiotic_x_set, row.names = NULL)
       }
-      
-      
-      
+
+
+
       antibiotic_x_set <- full_join(x= mono_antibiotic_x_set,y = comb_antibiotic_x_total) #generate full antibiotic dataset for analysis
       antibiotic_x_set <- select(antibiotic_x_set, starts_with("ab"),starts_with("namerx"))
-      
+
     } else {
       antibiotic_x_set <- mono_antibiotic_x_set
     }
     return(antibiotic_x_set)
   }
-  
+
   antibiotic_rx <- antibiotic_df(abo_in = antibiotic_in) #full dataframe with antibiotic regimens
-  
-  
+
+
   #if the dataset names are in different format to built-in set
   names(x) <- str_to_upper(names(x))
   if ((length(names(x)[sapply(names(x), function(x1){any(str_detect(x1,"[A-Z]$") & !(x1 %in% antimicrobials$ab) & !(x1 %in% antimicrobials$name))})])>0)){
@@ -62,30 +78,30 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
     names(x)[which(str_detect(names(x),"^WARD") & str_detect(names(x),"TYPE$"))] <- "ward"
     names(x)[which(str_detect(names(x),"^HOSP"))] <- "hospital"
     names(x)[which(str_detect(names(x),"DATE") & (str_detect(names(x),"REGISTRATION")))] <- "date"
-    
+
   }
-  
+
   #remove any duplicated variables
   if (any(duplicated(names(x)))){
     x <- x[,-max(which(names(x) == names(x)[which(duplicated(names(x)))]))]
   } else {
     x <- x
   }
-  
+
   x <- arrange(x,"date") #arrange in increasing date
-  
+
   #if your dataset does not have an episode identifier
   if (length(names(x)[sapply(names(x), function(x1){any(str_detect(x1,"patient"))})]) == 0){
     x$patient <- seq(1,nrow(x),1)
   }
-  
-  
-  
+
+
+
   #find demographic data within dataset
   micro_df_ind <- which(sapply(x,function(y) any(str_detect(y,"^S$"))|any(str_detect(y,"^R$"))|any(str_detect(y,"SENSITIVE"))| any(str_detect(y,"RESISTANT"))) ==  TRUE)
   micro_df <-  names(x)[micro_df_ind]#column names for susceptibility data
   demograph_df <- names(x)[which(!names(x) %in% micro_df)] #column names for demographic or organism data
-  
+
   #Dataset required to contain both mo and fullname
   if ("mo" %in% names(x)){
     # If mo included but not fullname:
@@ -93,7 +109,7 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
       mutate(mo = as.mo(mo))
     test_isolates_pre <- x %>%
       mutate(across(.cols = all_of(demograph_df),~ str_to_title(.x))) %>%
-      mutate(mo = toupper(mo)) 
+      mutate(mo = toupper(mo))
     if ("fullname" %in% names(x)){
       test_isolates <- test_isolates_pre[,c(demograph_df)]
     } else {
@@ -102,17 +118,17 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
     test_isolates <- test_isolates %>% #remove bugs to be excluded and final style conversions
       filter(!(fullname %in% exclude)) %>%
       mutate(fullname = str_to_title(fullname))
-    
+
     rx_abo <- x %>% #from orginal dataset
       filter(mo %in% test_isolates$mo)  %>% #select only those bugs that should be included
       distinct(date, patient, mo, .keep_all = TRUE) #ensure that there are no duplications
     rx_regs <- names(rx_abo)[!(names(rx_abo) %in% demograph_df)] #extract susceptibility data
-    
+
     if (any(str_detect(rx_regs,fixed("+")))){#if combinations have already been accounted for in susceptibility profiling
       rx_regs_ind <- which(!(names(rx_abo) %in% demograph_df)) # extract susceptibility data
       rx_regs_comb <- rx_regs[str_detect(rx_regs,fixed("+"))]# find combination regimen variables
       rx_regs_comb_x_total <- data.frame()
-      
+
       for (jj in 1:length(rx_regs_comb)){ # for every combination regimen
         rx_regs_comb_sep <- str_remove_all(unlist(str_split(rx_regs_comb[jj], fixed("+"))),"[[:punct:]]") #extract separate antibiotics included in regimen
         rx_regs_comb_pre <- data.frame(x = rep(0, times = 2), y = rep(0, times = 2))
@@ -131,7 +147,7 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
         names(rx_regs_comb_x_set)[ncol(rx_regs_comb_x_set)]<- "namerx"
         rx_regs_comb_x_total <-rbind.data.frame(rx_regs_comb_x_total, rx_regs_comb_x_set, row.names = NULL)
       }
-      
+
       rx_regs_comb_rename <- select(rx_regs_comb_x_total,starts_with("ab")) #extract all antibiotic abbreviations
       #regenerate variable names for dataset
       suppressWarnings({
@@ -139,20 +155,20 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
           rx_regs_comb_rename_2 <- unlist(rx_regs_comb_rename[k,])
           rx_regs_comb_rename_vec <- str_flatten(str_c(rx_regs_comb_rename_2, collapse = "+"))
           names(rx_abo)[names(rx_abo) == rx_regs_comb[k]] <- rx_regs_comb_rename_vec
-          
+
         }
       })
-      names(rx_abo)[!(str_detect(names(rx_abo),fixed("+")))  & !(names(rx_abo) %in% demograph_df)] <- 
+      names(rx_abo)[!(str_detect(names(rx_abo),fixed("+")))  & !(names(rx_abo) %in% demograph_df)] <-
         as.ab(names(rx_abo)[!(str_detect(names(rx_abo),fixed("+")))  & !(names(rx_abo) %in% demograph_df)])
       names(rx_abo)[which(duplicated(names(rx_abo)))] <- str_c(names(rx_abo)[which(duplicated(names(rx_abo)))],".x")
       rx_abo_upd <- left_join_microorganisms(x = rx_abo, by = "mo")[,c(names(rx_abo),"fullname")]
-      
+
     } else {
       names(rx_abo)[!(names(rx_abo) %in% demograph_df)] <- as.ab(names(rx_abo)[!(names(rx_abo) %in% demograph_df)])
       names(rx_abo)[which(duplicated(names(rx_abo)))] <- str_c(names(rx_abo)[which(duplicated(names(rx_abo)))],".x")
       rx_abo_upd <- left_join_microorganisms(x = rx_abo, by = "mo")[,c(names(rx_abo),"fullname")]
-      
-    } 
+
+    }
   } else {
     #If fullname included in dataset:
     test_isolates_pre <- x %>%
@@ -160,20 +176,20 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
       filter(!(fullname %in% unique(exclude)))
     print(nrow(test_isolates_pre))
     test_isolates <- left_join_microorganisms(x = test_isolates_pre, by = "fullname")[,c(demograph_df,"mo")]
-    test_isolates$fullname <- str_to_title(test_isolates$fullname) 
+    test_isolates$fullname <- str_to_title(test_isolates$fullname)
     test_isolates$mo <- toupper(test_isolates$mo)
-    
+
     rx_abo <- x %>%
       mutate(fullname = str_to_title(fullname)) %>%
       filter(!(fullname %in% unique(exclude)))  %>%
       distinct(date, patient, fullname, .keep_all = TRUE)
     rx_regs <- names(rx_abo)[!(names(rx_abo) %in% demograph_df)] #susceptibility data
-    
+
     if (any(str_detect(rx_regs,fixed("+")))){#if combinations have already been accounted for
       rx_regs_ind <- which(!(names(rx_abo) %in% demograph_df))
       rx_regs_comb <- rx_regs[str_detect(rx_regs,fixed("+"))]
       rx_regs_comb_x_total <- data.frame()
-      
+
       for (jj in 1:length(rx_regs_comb)){
         rx_regs_comb_sep <- str_remove_all(unlist(str_split(rx_regs_comb[jj], fixed("+"))),"[[:punct:]]")
         rx_regs_comb_pre <- data.frame(x = rep(0, times = 2), y = rep(0, times = 2))
@@ -192,9 +208,9 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
         names(rx_regs_comb_x_set)[ncol(rx_regs_comb_x_set)]<- "namerx"
         rx_regs_comb_x_total <-rbind.data.frame(rx_regs_comb_x_total, rx_regs_comb_x_set, row.names = NULL)
       }
-      
+
       rx_regs_comb_rename <- select(rx_regs_comb_x_total,starts_with("ab"))
-      
+
       suppressWarnings({
         for (k in 1:nrow(rx_regs_comb_x_total)){
           rx_regs_comb_rename_2 <- unlist(rx_regs_comb_rename[k,])
@@ -202,23 +218,23 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
           names(rx_abo)[names(rx_abo) == rx_regs_comb[k]] <- rx_regs_comb_rename_vec
         }
       })
-      names(rx_abo)[!(str_detect(names(rx_abo),fixed("+"))) & !(names(rx_abo) %in% demograph_df)] <- 
+      names(rx_abo)[!(str_detect(names(rx_abo),fixed("+"))) & !(names(rx_abo) %in% demograph_df)] <-
         as.ab(names(rx_abo)[!(str_detect(names(rx_abo),fixed("+"))) & !(names(rx_abo) %in% demograph_df)])
       names(rx_abo)[which(duplicated(names(rx_abo)))] <- str_c(names(rx_abo)[which(duplicated(names(rx_abo)))],".x")
       rx_abo_upd <- left_join_microorganisms(x = rx_abo, by = "fullname")[,c(names(rx_abo),"mo")]
-      
+
     } else {
       names(rx_abo)[!(names(rx_abo) %in% demograph_df)] <- as.ab(names(rx_abo)[!(names(rx_abo) %in% demograph_df)])
       names(rx_abo)[which(duplicated(names(rx_abo)))] <- str_c(names(rx_abo)[which(duplicated(names(rx_abo)))],".x")
       rx_abo_upd <- left_join_microorganisms(x = rx_abo, by = "fullname")[,c(names(rx_abo),"mo")]
-      
-    } 
+
+    }
   }
-  
-  
+
+
   rx_abo_upd$fullname <- str_to_title(rx_abo_upd$fullname)
-  
-  
+
+
   #generate new dataset with demographic information and only antibiotics under investigation
   rx_1 <- select(antibiotic_rx,starts_with("ab")) #all antibiotics
   rx_df <- test_isolates
@@ -226,7 +242,7 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
   rx_df[[var_date]] <- ymd(rx_df[[var_date]])
   var_patient <- names(rx_df)[str_detect(names(rx_df),"patient")]
   rx_df[[var_patient]] <- toupper(rx_df[[var_patient]])
-  
+
   names_vec <- vector()
   suppressWarnings({
     for (k in 1:nrow(rx_1)){
@@ -264,8 +280,8 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
       }
     }
   })
-  
-  
+
+
   if (any(is.na(rx_df$mo))){
     missing1 <- which(is.na(rx_df$mo))
     if (rx_df[missing1,"fullname"] != "NA"){
@@ -274,22 +290,22 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
       rx_df <- rx_df[-missing1,]
     }
   }
-  
+
   rx_df <- rx_df %>%
     mutate(across(.cols = everything(),.fns = toupper))
-  
-  #Generate adapted first isolate dataset on antimicrobial level 
+
+  #Generate adapted first isolate dataset on antimicrobial level
   if (isolate_first == "yes"){ #if dataset has already been deduplicated for first isolates
     test_isolates_first <- rx_df
   } else {
-    test_isolates_first <- rx_df[first_isolate(x = rx_df, 
-                                               method = "e", 
-                                               episode_days = 14, 
+    test_isolates_first <- rx_df[first_isolate(x = rx_df,
+                                               method = "e",
+                                               episode_days = 14,
                                                col_date = var_date,
                                                col_mo = "mo") == TRUE,] #Only include isolates for microorganisms < 14 days apart
   }
-  
-  
+
+
   test_isolates_first_upd <- test_isolates_first %>%
     pivot_longer(cols = names(test_isolates_first)[which(sapply(test_isolates_first,
                                                                 function(y) any(str_detect(y,"^S$"))|any(str_detect(y,"^R$"))|any(str_detect(y,"^SENSITIVE$"))| any(str_detect(y,"^RESISTANT$"))))],
@@ -299,8 +315,8 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
     mutate(antibiogram = ifelse(antibiogram == "RESISTANT","R",antibiogram)) %>%
     mutate(antibiogram = factor(antibiogram),
            keyantimicrobials = as.factor(keyantimicrobials))
-  
-  
+
+
   #recode susceptibility of regimes if not already in the dataset: if any S, then all S; if any NA and not S, then combination NA; if all R, then combination R.
   if (any(duplicated(names(test_isolates_first_upd)))){
     test_isolates_temp3 <- test_isolates_upd %>%
@@ -318,39 +334,39 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
       rename("antibiogram" = "combiantibiogram") %>%
       mutate(antibiogram = ifelse(antibiogram == "U",NA_character_, antibiogram)) %>%
       mutate(antibiogram = as.factor(antibiogram))
-    
+
     test_isolates_temp5 <- test_isolates_upd %>% #isolate the single regimens
       group_by(date,patient,mo) %>%
       filter(!str_detect(keyantimicrobials,fixed("+"))) %>%
       ungroup()
-    
+
     test_isolates_final <- bind_rows(test_isolates_temp4,test_isolates_temp5)
     test_isolates_final <- test_isolates_final %>%
-      arrange(patient) 
-    
+      arrange(patient)
+
   } else {
     test_isolates_final <- test_isolates_first_upd
-  }        
-  
+  }
+
   test_isolates_final$fullname <- as.factor(test_isolates_final$fullname)
   test_isolates_final$date <- ymd(test_isolates_final$date)
-  
+
   #Calculate pathogen incidence and susceptible by regimen
   mo_name_total <- test_isolates_final %>% #deduplicate pathogen list
-    filter(!duplicated(mo)) %>% 
+    filter(!duplicated(mo)) %>%
     select(mo,fullname)
-  
+
   organism_count_total <- test_isolates_final %>% #count the number of occurrences of pathogens within first isolates set
     group_by(mo) %>%
     distinct(date,patient,.keep_all = TRUE) %>%
     count(mo) %>%
     ungroup() %>%
     arrange(desc(n))
-  
+
   organism_count_total_upd <- organism_count_total %>%
     mutate(mo = as.character(mo)) %>%
     mutate(mo = ifelse(str_detect(mo, "^B_KLBSL"),"B_KLBSL",mo)) %>%
-    mutate(mo = as.factor(mo)) 
+    mutate(mo = as.factor(mo))
   klebsiella_combine <- organism_count_total_upd %>%
     filter(mo == "B_KLBSL") %>%
     summarise(n_kleb = sum(n))
@@ -359,13 +375,13 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
     distinct(mo,.keep_all = TRUE)
   organism_count_total_upd <- rename(organism_count_total_upd,
                                      "n_inc" = "n")
-  
+
   regimen_organism <- slice_max(organism_count_total_upd, order_by = n_inc, n = pathogen_in) #choose only the top n number of occurring pathogens to consider in WISCA
   total_pathogens <- sum(regimen_organism$n_inc) #total number of occurrences of pathogens
   print(paste0("Total pathogens = ",sum(organism_count_total_upd$n_inc)))
   print(paste0("Number of pathogens contributing to WISCA = ", total_pathogens))
   print(paste0("Percentage of pathogens contributing to WISCA = ",round((total_pathogens/sum(organism_count_total_upd$n_inc))*100,0),"%"))
-  
+
     test_isolates_final <- test_isolates_final %>%
     mutate(mo = as.character(mo)) %>%
     mutate(mo = ifelse(str_detect(mo, "^B_KLBSL"), "B_KLBSL",mo)) %>% #combine all Klebsiella species together
@@ -374,11 +390,11 @@ wisca_preprocess <- function(x, antibiotic_in,pathogen_in, exclude, susceptible_
     mutate(fullname = ifelse(str_detect(fullname, "Klebsiella"), "Klebsiella sp.", fullname)) %>% #rename all Klebsiella species
     mutate(fullname = as.factor(fullname)) %>%
     filter(mo %in% regimen_organism$mo)  #only keep top 10 pathogens
-  
-  test_isolates_final_upd <- test_isolates_final %>% 
+
+  test_isolates_final_upd <- test_isolates_final %>%
     mutate(antibiogram = as.sir(antibiogram)) %>%
     pivot_wider(names_from = "keyantimicrobials", values_from = "antibiogram")
-  
+
   preprocess_out <- list(names_vec,test_isolates_final_upd)
   return(preprocess_out)
 }
